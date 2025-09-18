4596b58d568631bf5a7983537079c1c98a7b4ab3
SaadAli2004 <mohammadsaadali16@gmail.com>
2025-09-18 19:06:57 +0500

    Added Image search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import { Masonry } from "@mui/lab";
 import { BiSearch } from "react-icons/bi";
 
 import axios from "axios";
+
 function App() {
+  const [userInput, setUserInput] = useState("");
   const [images, setImages] = useState([]);
   const key = import.meta.env.VITE_PHOTO_API;
   const url = "https://api.unsplash.com/";
+
+  
+  const searchHandler = async(input) => {
+    try {
+      const photos = await axios.get(
+        `${url}/search/photos?query=${input}&page=1&per_page=20&client_id=${key}`
+      );
+
+      setImages(photos.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
+
     const fetchData = async () => {
       try {
         const photos = await axios.get(
@@ -24,30 +40,34 @@ function App() {
   }, []);
   return (
     <>
-    <div className="overflow-hidden">
-
-
-      <div className="text-3xl gap-2 text-center p-3">
-        <h1 className=" p-2 font-bold">Enter Image Title</h1>
-        <div className="flex gap-2 items-center">
-        <input onChange={() => console.log(e.target.value)}type="text" className="bg-red-400 rounded-md text-lg p-2 w-full" placeholder="Here" />
-        <button onClick={() => console.log("click")}>
-          <BiSearch/>
-          </button>
-        </div>
-        
-      </div>
-
-      <Masonry columns={{xs:2, sm:2 , md:4, lg:4}} spacing={{xs:2, lg:2}} className="flex justify-evenly">
-        {images.map((img,index) => (
-          <div className="border" key={index}>
-            <img src={img.urls.small} alt="" />
+      <div className="overflow-hidden">
+        <div className="text-3xl gap-2 text-center p-3">
+          <h1 className=" p-2 font-bold">Enter Image Title</h1>
+          <div className="flex gap-2 items-center">
+            <input
+              onChange={(e) => setUserInput(e.target.value)}
+              type="text"
+              className="bg-red-400 rounded-md text-lg p-2 w-full"
+              placeholder="Here"
+            />
+            <button onClick={() => searchHandler(userInput)} className="cursor-pointer">
+              <BiSearch />
+            </button>
           </div>
-        ))}
-      </Masonry>
-    </div>
+        </div>
 
-      
+        <Masonry
+          columns={{ xs: 2, sm: 2, md: 4, lg: 4 }}
+          spacing={{ xs: 2, lg: 2 }}
+          className="flex justify-evenly"
+        >
+          {images.map((img, index) => (
+            <div className="border" key={index}>
+              <img src={img.urls.small} alt="" />
+            </div>
+          ))}
+        </Masonry>
+      </div>
     </>
   );
 }
