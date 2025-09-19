f719c9a9d3e8bf3c926db920319cb1bd9cc45890
SaadAli2004 <mohammadsaadali16@gmail.com>
2025-09-19 08:34:23 +0500

    Removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
-import { Box } from "@mui/material";
 import { Masonry } from "@mui/lab";
 import { BiSearch } from "react-icons/bi";
 import { Modal } from "@mui/material";
-
 import axios from "axios";
 
 function App() {
